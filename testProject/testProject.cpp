893fb5b5e698c571aca45498d7f60289cd190bce
KiimEviL <kimasia3000@gmail.com>
2024-12-23 21:31:14 +0700

    Добавьте файлы проекта.

---
+++ testProject/testProject.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+using namespace std;
+
+class Player {
+private:
+	string pName;
+	Player* pNext;
+public:
+	Player(string name = "") :pName(name), pNext(0){}
+
+	Player* getNext() {
+		return pNext;
+	}
+	string getName() {
+		return pName;
+	}
+	void setNext(Player* next) {
+		pNext = next;
+	}
+};
+
+class Party {
+private:
+	Player* lHead;
+public:
+	Party() :lHead(0) {}
+
+	void addPlayer(string name) {
+		Player* pNew = new Player(name);
+		if (lHead == 0) {
+			lHead = pNew;
+		}
+		else {
+			while (lHead->getNext()) {
+				lHead = lHead->getNext();
+			}
+			lHead->setNext(pNew);
+		}
+		//else {
+		//	Player* pTemp = lHead;
+		//	while (pTemp->getNext()) {
+		//		pTemp = pTemp->getNext();
+		//	}
+		//	pTemp->setNext(pNew);
+
+		//}
+
+
+	}
+
+	void showAll() {
+		if (lHead == 0) {
+			cout << "Empty" << endl;
+		}
+		else
+			while (lHead) {
+				cout << lHead->getName() << endl;
+				lHead = lHead->getNext();
+			}
+	}
+};
+
+int main() {
+	Party l;
+	l.addPlayer("aaa");
+	l.addPlayer("bbb");
+	l.addPlayer("sss");
+	l.addPlayer("ddd");
+	l.showAll();
+
+	return 0;
+}
